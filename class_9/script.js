7577979d7b017adcb612c0af233c026e3c8e4b7e
Jatinpatidar768 <140938416+Jatinpatidar768@users.noreply.github.com>
2025-12-03 22:25:02 +0530

    Add files via upload

---
+++ class_9/script.js
@@ -0,0 +1,67 @@
+// let rev=0;
+// let num=325;
+// while(num!=0){
+//     rem=num%10;
+//     rev=rev*10+rem;
+//     num= Math.floor(num/10)
+// }
+// console.log(rev);
+
+
+
+// let rev=0;
+// let num=101010101015;
+// let jat=num;
+// while(num>0){
+//     rem=num%10;
+//     rev=rev*10+rem;
+//     num= Math.floor(num/10)
+// }
+
+// if(jat==rev){
+//     console.log("number is palindrom");
+    
+// }
+// else{
+//     console.log("number is not palindrom");
+    
+// }
+
+
+
+
+
+
+// let num= 12346;
+// let count=0;
+// while(num !=0)
+// {
+//    num =Math.floor (num/10);
+//    count ++;
+// }
+// console.log(`The number contains ${count} digits `);
+
+
+
+
+// let rev=0;
+// let num=142;
+// let n=num;
+// while(num!=0){
+//     rem=num%10;
+//     rev=rev*10+rem;
+//     num= Math.floor(num/10)
+  
+// }
+//   if(n%3==0){
+//       console.log(rev  + "is divisible of 3");
+      
+//     }
+//     else{
+//       console.log(rev + "is not divisible by 3");
+      
+//     }
+// console.log(rev);
+
+
+
