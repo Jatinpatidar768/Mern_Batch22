82e1ab147c98e6726844fda9ee7f5297bde87c07
Jatinpatidar768 <140938416+Jatinpatidar768@users.noreply.github.com>
2025-12-12 19:20:54 +0530

    API and closer

---
+++ JAVASCRIPT/classs_14/script.js
@@ -0,0 +1,34 @@
+
+// closer
+// function outer(){
+//     let initialpostcount=0;
+//     return function inner(){
+        
+//         console.log(initialpostcount);
+//         return initialpostcount++;
+        
+//     }
+
+// }
+// console.log("first");
+// let ob=null;
+// ob=outer();
+// console.log(ob());
+// console.log(ob());
+// console.log(ob());
+
+
+// console.log("second");
+// let ob1=outer()
+// console.log(ob1());
+// console.log(ob1());
+
+
+
+
+
+
+
+
+
+
