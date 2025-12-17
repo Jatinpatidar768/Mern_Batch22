31acfa6d3aac20a3a36501288c3bbbf8e02bf404
Jatinpatidar768 <140938416+Jatinpatidar768@users.noreply.github.com>
2025-12-17 22:19:38 +0530

    event and event handler in js and dom

---
+++ JAVASCRIPT/class_18/script.js
@@ -0,0 +1,30 @@
+// event and event handler in dom
+// 1 step get div so that you can attach btn
+// let divele=document.getElementById("div1");
+// console.log(("now we",divele));
+
+// // 2.crete a buttn
+// let addbtn=document.createElement("button");
+// console.log("abbe chutiye",addbtn);
+
+// // 3.add the content to the btn
+// addbtn.textContent="add";
+
+// // 4.attech the btn inside a dive
+// divele.appendChild(addbtn)
+
+
+// // task 2
+// // 1.ger the btn so we always btn(done)
+// // 2. attacheventhandler
+// addbtn.addEventListener("click",()=>{
+//     console.log("sari duniya jla denge");
+//     // 1.get div so that you can attach btn
+//     // 2. create paragraph
+//     let paraele=document.createElement("p");
+//     // 3. add content the btn
+//     paraele.textContent="tata by by"
+//     // 4. attch para to the div
+//     divele.appendChild(paraele); 
+    
+// })
